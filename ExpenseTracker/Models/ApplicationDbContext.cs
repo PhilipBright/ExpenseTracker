29e491141562c360facf46802d6a1775280aa205
Philip Bright <macbookpro2021@AppleClubs-MacBook-Pro.local>
2024-03-17 15:21:25 +0630

    first commit

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options):base(options)
+		{
+		}
+
+		public DbSet<Transation> Transations { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
+
---
+++ ExpenseTracker/Models/Transation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+	public class Transation
+	{
+		[Key]
+		public int TransactionId { get; set; }
+
+		//Category ID
+
+		public int CategoryId { get; set; }
+		public Category Category { get; set; }
+
+		public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+		public DateTime Time { get; set; } = DateTime.Now;
+
+	}
+}
+
